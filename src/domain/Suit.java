0aa9db806703359ecf9b1e3897e46162c9176701
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-18 15:10:24 +0200

    Enum Suit version 2 (toString añadido)

--- src/domain/Suit.java
+++ src/domain/Suit.java
@@ -5,5 +5,15 @@ public enum Suit {
 	GOLD, 	//oro
 	CUPS, 	//copas
 	SWORDS, //espadas
-	CLUBS 	//bastos
+	CLUBS; 	//bastos
+	
+	@Override
+	public String toString() {
+		return switch(this) {
+		case GOLD -> "Oro";
+		case CUPS -> "Copas";
+		case SWORDS -> "Espadas";
+		case CLUBS -> "Bastos";
+		};
+	}
 }
