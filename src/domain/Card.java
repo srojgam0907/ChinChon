57afd86bcb701a5930b3b84befd6126aa89578a4
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-18 15:11:22 +0200

    Clase Card version 1

--- src/domain/Card.java
+++ src/domain/Card.java
@@ -1,5 +1,40 @@
 package domain;
 
-public class Card {
+public class Card implements Comparable<Card>{
 
+	private final Suit suit;
+	private final Value value;
+	
+	public Card(Suit suit, Value value) {
+		this.suit= suit;
+		this.value= value;
+	}
+	
+	public Suit getSuit() {
+		return suit;
+	}
+	
+	public Value getValue() {
+		return value;
+	}
+	
+	public int getPoints() {
+		return value.getNumValue(); 
+	}
+	
+	@Override
+	public int compareTo(Card o) {
+		// Primero ordenamos por palo y luego por valor
+		
+		if (this.suit != o.suit) {
+			return this.suit.compareTo(o.suit);
+		}
+		
+		return Integer.compare(this.value.getNumValue(), o.value.getNumValue()); 
+	}
+
+	@Override
+	public String toString() {
+		return value + " de " + suit.toString();  
+	}
 }
