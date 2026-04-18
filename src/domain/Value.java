5d9834eb98c91897cb31d24e30c1ebc8ace0f761
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-18 14:52:09 +0200

    Enum para el valor de las cartas version 1

--- src/domain/Value.java
+++ src/domain/Value.java
@@ -2,4 +2,16 @@ package domain;
 
 public enum Value {
 
+	ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6),
+	SEVEN(7), JACK(10), KNIGHT(11), KING(12); 
+	
+	private final int numValue;
+	
+	Value(int numValue) {
+		this.numValue= numValue;
+	}
+	
+	public int getNumValue() {
+		return numValue; 
+	}
 }
