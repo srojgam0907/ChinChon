eaa28b4cbbd4cb9ba1f22b2f3700f587e63264ca
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-20 09:50:37 +0200

    Clase CardFactory version 1

--- src/domain/CardFactory.java
+++ src/domain/CardFactory.java
@@ -1,5 +1,26 @@
 package domain;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+
 public class CardFactory {
 
-}
+	public static Deque<Card> createFullDeck(int numberOfDecks) {
+		List<Card> cards= new ArrayList<>();
+		
+		for(int i=0; i < numberOfDecks; i++) {
+			for(Suit suit : Suit.values()) {
+				for(Value value : Value.values()) {
+					cards.add(new Card(suit, value));
+				}
+			}
+		}
+		
+		Collections.shuffle(cards);
+		
+		return new ArrayDeque<>(cards); 
+	}
+}                                 
