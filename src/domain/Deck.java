e6ec2053d395fe887079e7c257cb3c12e464ce84
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-21 09:45:35 +0200

    Clase deck version 1

--- src/domain/Deck.java
+++ src/domain/Deck.java
@@ -1,5 +1,44 @@
 package domain;
 
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+import java.util.Optional;
+
 public class Deck {
 
+	private Deque<Card> cards;
+	
+	public Deck(int numberOfDecks) {
+		this.cards= CardFactory.createFullDeck(numberOfDecks);
+	}
+	
+	public Optional<Card> drawCard() {
+		if(cards.isEmpty()) {
+			return Optional.empty(); 
+		}
+		
+		return Optional.of(cards.pop()); 
+	}
+	
+	public void refillFromDiscardPile(List<Card> discardedCards) {
+		Collections.shuffle(discardedCards);
+		
+		for(Card card : discardedCards) {
+			cards.push(card); 
+		}
+	}
+	
+	public boolean isEmpty() {
+		return cards.isEmpty();
+	}
+	
+	public int deckSize() { 
+		return cards.size();
+	}
+	
+	@Override
+    public String toString() {
+        return String.format("Mazo: %d cartas restantes.", cards.size());
+    }
 }
