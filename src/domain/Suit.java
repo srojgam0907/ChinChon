4550efae4d60985101321effe6634f04b9a37009
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-23 18:34:16 +0200

    Javadoc del Enum Suit

--- src/domain/Suit.java
+++ src/domain/Suit.java
@@ -1,18 +1,31 @@
 package domain;
 
+/**
+ * Representa los cuatro palos de la baraja española utilizados en el juego del Chinchón.
+ * Los palos disponibles son Oros, Copas, Espadas y Bastos.
+ * Cada palo tiene asociado un emoji para facilitar su visualización en la consola.
+ */
 public enum Suit {
 
-	GOLD("🪙"), 	//oro
-	CUPS("🍷"), 	//copas
-	SWORDS("⚔️"), //espadas
-	CLUBS("🦯"); 	//bastos
+	GOLD("🪙"), 	
+	CUPS("🍷"), 	
+	SWORDS("⚔️"), 
+	CLUBS("🦯"); 
 	
 	private final String emoji;
 	
+	/**
+     * Constructor del enum Suit.
+     * @param emoji Cadena de texto que contiene el emoji que representa al palo.
+     */
 	Suit(String emoji) {
 		this.emoji= emoji;
 	}
 	
+	/**
+     * Obtiene el emoji asociado a este palo.
+     * @return Una cadena con el emoji del palo.
+     */
 	public String getEmoji() {
 		return emoji;  
 	}
