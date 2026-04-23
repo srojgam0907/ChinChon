10f0b15bce7dafe3eac2f72f3984fc3351b2eb1b
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-23 18:28:59 +0200

    Clase discardPile version 1

--- src/domain/DiscardPile.java
+++ src/domain/DiscardPile.java
@@ -1,5 +1,68 @@
 package domain;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Optional;
+
 public class DiscardPile {
 
+	public Deque<Card> discardedCards;
+	
+	public DiscardPile() {
+		this.discardedCards= new ArrayDeque<>();
+	}
+	
+	public void pushCard(Card card) {
+		discardedCards.push(card);
+	}
+	
+	public Optional<Card> peekLastCard() {
+		if(discardedCards.isEmpty()) {
+			return Optional.empty();
+		}
+		
+		return Optional.of(discardedCards.peek());
+	}
+	
+	public Optional<Card> takeCard() {
+		if(discardedCards.isEmpty()) {
+			return Optional.empty();
+		}
+		
+		return Optional.of(discardedCards.pop()); 
+	}
+	
+	public List<Card> collectAllButLast() {
+		List<Card> reUse= new ArrayList<>();
+		Card topCard;
+		
+		if(discardedCards.size() <= 1) {
+			return reUse;
+		}
+		
+		topCard= discardedCards.pop();
+		
+		reUse.addAll(discardedCards);
+		
+		discardedCards.clear();
+		discardedCards.push(topCard);
+		
+		return reUse; 
+	}
+	
+	public boolean isEmpty() {
+		return discardedCards.isEmpty();
+	}
+	
+	public int size() {
+		return discardedCards.size();
+	}
+	
+	@Override
+	public String toString() {
+		return discardedCards.isEmpty() ? "La pila de descartes está vacía" :
+				"Última carta en el descarte: " + discardedCards.peek(); 
+	}
 }
