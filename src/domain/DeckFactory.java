96eb8134daa96013cb84b2fe60ec44b7e1766388
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-24 12:02:01 +0200

    JavaDoc de la clase DeckFactory

--- src/domain/DeckFactory.java
+++ src/domain/DeckFactory.java
@@ -6,8 +6,19 @@ import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
 
+/**
+ * Factoría encargada de la creación y configuración inicial del mazo de cartas.
+ * Esta clase centraliza la lógica para generar barajas completas mezcladas y listas para jugar.
+ */
 public class DeckFactory {
 
+	/**
+     * Crea un mazo completo siguiendo las reglas de la baraja española (40 cartas por baraja).
+     * El mazo se genera combinando todos los palos y valores disponibles.
+     * Al finalizar la creación, las cartas se barajan aleatoriamente.
+     * @param numberOfDecks Número de barajas de 40 cartas que compondrán el mazo total. 
+     * @return Una Deque de objetos Card, barajada y organizada como una pila.
+     */
 	public static Deque<Card> createFullDeck(int numberOfDecks) {
 		List<Card> cards= new ArrayList<>();
 		
