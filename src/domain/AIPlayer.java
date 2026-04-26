b598bc11352167b33219c507f4086e11438314cf
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-26 18:02:18 +0200

    Clase AIPlayer version 1

--- src/domain/AIPlayer.java
+++ src/domain/AIPlayer.java
@@ -1,5 +1,42 @@
 package domain;
 
-public class AIPlayer {
+public class AIPlayer extends Player{
 
+	/**
+	 * Constructor del jugador IA
+	 * @param name variable heredada de Player
+	 */
+	public AIPlayer(String name) {
+		super(name);
+	}
+
+	@Override
+	public void playTurn(Deck deck, DiscardPile discardPile) {
+		boolean tookDiscard= false;
+		Card topCard;
+		Card toDiscard;
+		
+		System.out.printf("\n>>> TURNO DE LA IA: %s\n", name);
+		
+		if(!discardPile.isEmpty()) {
+			topCard= discardPile.peekLastCard().get();
+			
+			if(topCard.getPoints() <=5) {
+				discardPile.takeCard().ifPresent(hand::addCard);
+				System.out.printf("IA %s ha robado del descarte\n", name);
+				tookDiscard= true;
+			}
+		}
+		
+		if(!tookDiscard) {
+			deck.drawCard().ifPresent(hand::addCard);
+			System.out.printf("IA %s ha robado del mazo\n", name);
+		}
+		
+		hand.sortHand();
+		toDiscard= hand.discardCard(hand.size() -1);
+		discardPile.pushCard(toDiscard);
+		
+		System.out.printf("IA %s ha descartado: %s\n", name, toDiscard);
+	}
 }
