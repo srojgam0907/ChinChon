9c8ac09a36639a5770fe4cbbd55fa753b3f0af8b
srojgam0907 <srojgam0907@g.educaand.es>
2026-05-03 12:13:46 +0200

    Clase HumanPlayer version 2

--- src/domain/HumanPlayer.java
+++ src/domain/HumanPlayer.java
@@ -11,6 +11,7 @@ public class HumanPlayer extends Player{
 
 	private Scanner kb= new Scanner(System.in);
 	private ConsoleInput ci= ConsoleInput.getInstance(kb);
+	private boolean hasClosed;
 	
 	/**
 	 * Constructor del jugador humano
@@ -18,25 +19,55 @@ public class HumanPlayer extends Player{
 	 */
 	public HumanPlayer(String name) {
 		super(name);
+		hasClosed= false;
+	}
+
+	/**
+	 * True si ha cerrado, false si no ha cerrado
+	 * @return estado de cierre del jugador
+	 */
+	public boolean isHasClosed() {
+		return hasClosed;
 	}
 
 	@Override
 	public void playTurn(Deck deck, DiscardPile discardPile) {
-		boolean drawn;
-		boolean discarded;
-		boolean choice;
-		int cardPosition;
+		System.out.printf("\n====== TURNO DE: %s ======\n", name); 
 		
-		System.out.printf("\n=================================\nTURNO DE: %s\n=================================\n", name.toUpperCase());
-		
-		// Mostrar estado actual
+		//ESTADO ACTUAL
         System.out.println(discardPile.toString());
         hand.sortHand();
         System.out.println(hand.toString());
 		
         //ROBO
-        drawn= false;
+        executeDrawn(deck, discardPile);
+        hand.sortHand();
+        System.out.printf("\nHas robado. Tu mano actual es: %s\n", hand.toString());
         
+        //DESCARTE o CIERRE
+        if(hand.canClose()) {
+        	System.out.print("\n¡Puedes cerrar¡ ¿Quieres cerrar? [S/N]: ");
+        	
+        	if(ci.readBooleanUsingChar('s', 'n')) {
+        		executeCloseDiscard(discardPile);
+        		
+        	} else {
+        		executeNormalDiscard(discardPile);
+        		
+        	}
+        	
+        } else {
+        	executeNormalDiscard(discardPile); 
+        }
+	}
+	
+	/**
+	 * Ejecuta el robo del jugador del mazo o del descarte 
+	 */
+	private void executeDrawn(Deck deck, DiscardPile discardPile) {
+		boolean drawn= false;
+		boolean choice;
+		
         while(!drawn) {
         	System.out.println("¿Robas del [D]escarte o del [M]azo?:");
         	choice= ci.readBooleanUsingChar('d', 'm'); //d = true, m = false
@@ -47,29 +78,46 @@ public class HumanPlayer extends Player{
         			drawn= true;
         		
         		} else {
-        			System.out.println("La pila de descartes está vacía");
+        			System.out.println("La pila de descartes está vacía. Por favor elije el mazo");
         		}
-        	}else {
+        		
+        	} else {
         		deck.drawCard().ifPresent(hand::addCard);
         		drawn= true;
         	}
         }
-        
-        hand.sortHand();
-        System.out.printf("\nHas robado. Tu mano actual es:\n%s", hand.toString());
-        
-        //DESCARTE
-        discarded= false;
-        
-        while(!discarded) {
-        	System.out.print("Elige la posición de la carta que quieres descartar: ");
-        	cardPosition= ci.readIntInRange(1, hand.size());
-        	
-        	Card c= hand.discardCard(cardPosition -1);
-        	discardPile.pushCard(c);
-        	
-        	System.out.println("Has descartado: " + c.toString());
-        	discarded= true;
-        }
+	}
+	
+	/**
+	 * Ejecuta un descarte normal cuando todavia el jugador no puede cerrar
+	 */
+	private void executeNormalDiscard(DiscardPile discardPile) {
+		int cardPosition;
+		
+		System.out.print("\nElige la posición de la carta que quieres descartar: ");
+    	cardPosition= ci.readIntInRange(1, hand.size());
+    	
+    	Card c= hand.discardCard(cardPosition -1);
+    	discardPile.pushCard(c);
+    	
+    	System.out.println("Has descartado: " + c.toString());
+	}
+	
+	/**
+	 * Cuando el jugador puede cerrar se activa esta modo de descarte
+	 * para descartar la ultima carta antes de cerrar la ronda
+	 */
+	private void executeCloseDiscard(DiscardPile discardPile) {
+		int cardPosition;
+		Card card;
+		
+		System.out.println("Selecciona la carta que vas a soltar (según su numero de posicion): ");
+		cardPosition= ci.readIntInRange(1, hand.size());
+		
+		card= hand.discardCard(cardPosition -1);
+		discardPile.pushCard(card);
+		
+		this.hasClosed= true;
+		System.out.printf("Has cerrado la ronda con: %s", card);
 	}
 }
