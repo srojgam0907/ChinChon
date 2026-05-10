cf638d3ce8ce5026ab34b566ba23edd2a4bdde20
srojgam0907 <srojgam0907@g.educaand.es>
2026-05-10 13:17:24 +0200

    Main version 1

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,8 +3,37 @@ package app;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Main().game();
 
 	}
+	
+	public void game() {
+		System.out.println("******************************************");
+		System.out.println("      BIENVENIDO AL JUEGO DEL CHINCHÓN    ");
+		System.out.println("******************************************");
 
+		GameEngine game= new GameEngine();
+        
+		game.configurationGame();
+        game.setUpGame();
+        game.startGame();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
